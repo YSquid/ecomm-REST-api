f31a82e7b88f438c1038c98e7a9dc1dc9f19062c
Ysquid <ahk648@gmail.com>
2023-01-07 12:24:05 -0600

    example data isnerted for orders and linking categories with products

--- db/test_queries.sql
+++ db/test_queries.sql
@@ -1,4 +1,4 @@
---three test products
+--five test products
 INSERT INTO products (name, description, price, stock)
 VALUES ('baseball bat', '28 inch wooden baseball bat', 39.99, 25);
 
@@ -7,6 +7,14 @@ VALUES ('basketball', 'official size 7 (29.5 inch) basketball', 35.99, 16);
 
 INSERT INTO products (name, description, price, stock)
 VALUES ('Football', 'NFL sized supergrip football', 28.99, 50);
+
+INSERT INTO products (name, description, price, stock)
+VALUES ('Electronic Scoreboard', 'An electronic scoreboard with LED display. Scores up to 999 per team', 99.99, 10);
+
+INSERT INTO products (name, description, price, stock)
+VALUES ('Whistle', 'Stainless steel whistle.', 4.99, 50);
+
+
 
 --three test categories
 
@@ -34,3 +42,47 @@ VALUES ('LaShonda', 'Williamson', '1930 Nootka Street', 'Vancouver', 'BC', 'CA')
 
 INSERT INTO carts (id)
 VALUES (1);
+
+--five test orders
+
+INSERT INTO orders(user_id)
+VALUES (1);
+
+INSERT INTO orders(user_id)
+VALUES (1);
+
+INSERT INTO orders(user_id)
+VALUES (2);
+
+INSERT INTO orders(user_id)
+VALUES (2);
+
+INSERT INTO orders(user_id)
+VALUES (3);
+
+--map catories to products with linking table
+INSERT INTO categories_products (category_id, product_id)
+VALUES(1, 1);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(2, 2);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(3, 3);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(1, 4);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(2, 4);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(3, 4);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(3, 5);
+
+INSERT INTO categories_products (category_id, product_id)
+VALUES(2, 5);
+
+
