92410b5cb6bde2c820372acf40a891d1e0e3a00f
Ysquid <ahk648@gmail.com>
2023-01-07 11:13:50 -0600

    fixed wrong paraenthesis for all

--- db/db_setup.sql
+++ db/db_setup.sql
@@ -1,18 +1,18 @@
-CREATE TABLE products {
+CREATE TABLE products (
     id SERIAL PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
     description VARCHAR(128),
     price MONEY NOT NULL,
     stock INTEGER NOT NULL
-}
+)
 
-CREATE TABLE categories {
+CREATE TABLE categories (
     id SERIAL PRIMARY KEY,
     name VARCHAR(32) NOT NULL,
     description VARCHAR(128) NOT NULL
-}
+)
 
-CREATE TABLE users {
+CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     cart_id INTEGER REFERENCES carts(id) NOT NULL,
     first_name VARCHAR(32) NOT NULL,
@@ -20,38 +20,38 @@ CREATE TABLE users {
     address VARCHAR(256),
     province_state CHAR(2),
     country CHAR(2),
-}
+)
 
-CREATE TABLE orders {
+CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id)
-}
+)
 
-CREATE TABLE carts {
+CREATE TABLE carts (
     id SERIAL PRIMARY KEY,
     user_id INTEGER REFERENCES users(id) NOT NULL;
-}
+)
 
-CREATE TABLE categories_products {
+CREATE TABLE categories_products (
     category_id INTEGER REFERENCES categories(id) NOT NULL,
     product_id INTEGER REFERENCES products(id) NOT NULL,
     PRIMARY KEY (category_id, product_id)
-}
+)
 
-CREATE TABLE orders_users {
+CREATE TABLE orders_users (
     order_id INTEGER REFERENCES orders(id) NOT NULL,
     users_id INTEGER REFERENCES users(id) NOT NULL,
     PRIMARY KEY (order_id, users_id)
-}
+)
 
-CREATE TABLE orders_products {
+CREATE TABLE orders_products (
     order_id INTEGER REFERENCES orders(id) NOT NULL,
     product_id INTEGER REFERENCES products(id) NOT NULL,
     PRIMARY KEY (order_id, product_id)
-}
+)
 
-CREATE TABLE carts_products {
+CREATE TABLE carts_products (
     cart_id INTEGER REFERENCES carts(id) NOT NULL,
     product_id INTEGER REFERENCES product(id) NOT NULL,
     PRIMARY KEY (cart_id, product_id)
-}
+)
