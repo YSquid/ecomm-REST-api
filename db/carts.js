241c3596608bee53339ac967e9e5bd0cb766e3b7
Ysquid <ahk648@gmail.com>
2023-01-13 13:38:54 -0600

    route for handling add product to cart (carts_products entry) done/tested

--- db/carts.js
+++ db/carts.js
@@ -28,20 +28,39 @@ const getCartById = (req, res, next) => {
 
 //POST actions
 
-const addCart = (req, res, next) => {
-  const { user_id } = req.body;
+//NOT NEEDED - CARTS AUTO ADDED WITH TRIGGER ON USER ADD
+// const addCart = (req, res, next) => {
+//   const { user_id } = req.body;
+//   pool.query(
+//     `INSERT INTO carts (user_id) VALUES ($1) RETURNING id, user_id;`,
+//     [user_id],
+//     (error, results) => {
+//       if (error) {
+//         next(error);
+//       } else {
+//         res.status(201).send(results.rows);
+//       }
+//     }
+//   );
+// };
+
+//Add item to a cart using query params
+
+const addProductToCart = (req, res, next) => {
+  const {cart_id, product_id} = req.query
   pool.query(
-    `INSERT INTO carts (user_id) VALUES ($1) RETURNING id, user_id;`,
-    [user_id],
+    `INSERT INTO carts_products (cart_id, product_id) VALUES ($1, $2) 
+    RETURNING cart_id, product_id;`,
+    [cart_id, product_id],
     (error, results) => {
       if (error) {
-        next(error);
+        next(error)
       } else {
-        res.status(201).send(results.rows);
+        res.status(201).send(results.rows)
       }
     }
-  );
-};
+  )
+}
 
 //PUT actions
 
@@ -82,7 +101,7 @@ const deleteCart = (req, res, next) => {
 module.exports = {
   getCarts,
   getCartById,
-  addCart,
+  addProductToCart,
   updateCart,
   deleteCart
 };
--- routes/cartsRouter.js
+++ routes/cartsRouter.js
@@ -11,7 +11,7 @@ cartsRouter.get("/", db_carts.getCarts);
 cartsRouter.get("/:id", db_carts.getCartById);
 
 //POST cart
-cartsRouter.post("/", db_carts.addCart);
+cartsRouter.post("/", db_carts.addProductToCart);
 
 //PUT cart
 cartsRouter.put("/:id", db_carts.updateCart);
