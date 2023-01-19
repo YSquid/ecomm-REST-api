ebd8200250d2a297579f71fd1d8de2e220a0d3e0
Ysquid <ahk648@gmail.com>
2023-01-19 15:54:48 -0600

    added test for registration redirecting to login

--- app.test.js
+++ app.test.js
@@ -4,25 +4,42 @@ const baseURL = "http://localhost:3000";
 //Homepage testing
 
 describe("Homepage Tests /", () => {
-  it("GET '/' should return 200 status", async () => {
+  it("GET '/' should return 200 status and Hi {name}", async () => {
     const response = await request(baseURL).get("/");
     expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("<h1>Hi Ahmad</h1>");
   });
 });
 
+//Registration Tests
 
-//Registration Testing
-
-// describe("Registration Tests", () => {
-//   it("Get '/reg'")
-// })
+describe("Registration Tests", () => {
+  describe("Registration Page Render", () => {
+    it("GET '/register' should return 200 status and register page", async () => {
+      const response = await request(baseURL).get("/register");
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toContain("<h1>Register</h1>");
+    });
+  });
+  describe("Register New User Tests", () => {
+    it ("POST 'register' should redirect to /login on successful registration", async () => {
+      const response = await request(baseURL)
+      .post("/register")
+      .send({
+        email: 'testsuite5@supertest.com',
+        password: 'test'
+      })
+      expect(response.header.location).toBe('/login')
+    })
+  })
+});
 
 //Login testing
 describe("Login Tests", () => {
-  it("GET '/login' should return 200 status", async () => {
+  it("GET '/login' should return 200 status and login page", async () => {
     const response = await request(baseURL).get("/login");
     expect(response.statusCode).toBe(200);
-    expect(response.text).toContain('<h1>Login</h1>')
+    expect(response.text).toContain("<h1>Login</h1>");
   });
 
   it("POST '/login' should redirect to /api on successful login", async () => {
@@ -36,24 +53,19 @@ describe("Login Tests", () => {
   });
 
   it("POST '/login' should return 'Found. Redirecting to /api' on successful login", async () => {
-    const response = await request(baseURL)
-    .post('/login')
-    .send({
-        email: "a@a",
-        password: "123",
-      })
+    const response = await request(baseURL).post("/login").send({
+      email: "a@a",
+      password: "123",
+    });
 
-    expect(response.text).toBe("Found. Redirecting to /api")
-    })
-
+    expect(response.text).toBe("Found. Redirecting to /api");
+  });
 
   it("POST '/login' should return 'Unauthorized' on unsuccessful login", async () => {
-    const response = await request(baseURL)
-      .post("/login")
-      .send({
-        email: "a@a",
-        password: "qwe",
-      })
-      expect(response.text).toBe("Unauthorized")
+    const response = await request(baseURL).post("/login").send({
+      email: "a@a",
+      password: "qwe",
+    });
+    expect(response.text).toBe("Unauthorized");
   });
 });
