167cea39537dac154349be1ada9faba333c3f773
Ysquid <ahk648@gmail.com>
2023-01-23 11:23:36 -0600

    cleanup after merge

--- app.test.js
+++ app.test.js
@@ -1,58 +1,57 @@
 const request = require("supertest");
 const baseURL = "http://localhost:3000";
-var server = request.agent("http://localhost:3000");
 const superagent = require("superagent").agent();
 
-//Homepage testing
+// Homepage testing
 
-// describe("Homepage Tests /", () => {
-//   it("GET '/' should return 200 status and Hi {name}", async () => {
-//     const response = await request(baseURL).get("/");
-//     expect(response.statusCode).toBe(200);
-//     expect(response.text).toBe("<h1>Hi Ahmad</h1>");
-//   });
-// });
+describe("Homepage Tests /", () => {
+  it("GET '/' should return 200 status and Hi {name}", async () => {
+    const response = await request(baseURL).get("/");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toBe("<h1>Hi Ahmad</h1>");
+  });
+});
 
-// //Registration Tests
+//Registration Tests
 
-// describe("Registration Tests", () => {
-//   describe("Registration Page Render", () => {
-//     it("GET '/register' should return 200 status and register page", async () => {
-//       const response = await request(baseURL).get("/register");
-//       expect(response.statusCode).toBe(200);
-//       expect(response.text).toContain("<h1>Register</h1>");
-//     });
+describe("Registration Tests", () => {
+  describe("Registration Page Render", () => {
+    it("GET '/register' should return 200 status and register page", async () => {
+      const response = await request(baseURL).get("/register");
+      expect(response.statusCode).toBe(200);
+      expect(response.text).toContain("<h1>Register</h1>");
+    });
 
-//     it("Should log in then delete a user", async () => {});
-//   });
-// });
+    it("Should log in then delete a user", async () => {});
+  });
+});
 
-// //Login testing
-// describe("Login Tests", () => {
-//   it("GET '/login' should return 200 status and login page", async () => {
-//     const response = await request(baseURL).get("/login");
-//     expect(response.statusCode).toBe(200);
-//     expect(response.text).toContain("<h1>Login</h1>");
-//   });
+//Login testing
+describe("Login Tests", () => {
+  it("GET '/login' should return 200 status and login page", async () => {
+    const response = await request(baseURL).get("/login");
+    expect(response.statusCode).toBe(200);
+    expect(response.text).toContain("<h1>Login</h1>");
+  });
 
-//   it("POST '/login' should redirect to /api on successful login", async () => {
-//     await request(baseURL)
-//       .post("/login")
-//       .send({
-//         email: "a@a",
-//         password: "123",
-//       })
-//       .expect("Location", "/api");
-//   });
+  it("POST '/login' should redirect to /api on successful login", async () => {
+    await request(baseURL)
+      .post("/login")
+      .send({
+        email: "a@a",
+        password: "123",
+      })
+      .expect("Location", "/api");
+  });
 
-//   it("POST '/login' should return 'Unauthorized' on unsuccessful login", async () => {
-//     const response = await request(baseURL).post("/login").send({
-//       email: "a@a",
-//       password: "qwe",
-//     });
-//     expect(response.text).toBe("Unauthorized");
-//   });
-// });
+  it("POST '/login' should return 'Unauthorized' on unsuccessful login", async () => {
+    const response = await request(baseURL).post("/login").send({
+      email: "a@a",
+      password: "qwe",
+    });
+    expect(response.text).toBe("Unauthorized");
+  });
+});
 
 //Registration testing
 describe("Registration tests", () => {
