933342224fb4f92bd365f98875d18cbfbb36a8b3
Ysquid <ahk648@gmail.com>
2023-02-17 12:07:41 -0600

    ejs require

--- functions/app.js
+++ functions/app.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const bodyParser = require("body-parser");
 require("./passportConfig")(passport);
 const db_auth = require("./db/auth");
+let ejs = require('ejs');
 // const cors = require("cors");
 
 
