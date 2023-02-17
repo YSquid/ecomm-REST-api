cbba0fc921900e4f20827c0b305eb60cc0f3b250
Ysquid <ahk648@gmail.com>
2023-02-17 14:42:14 -0600

    adding cookie to login route

--- functions/app.js
+++ functions/app.js
@@ -90,6 +90,13 @@ app.post(
     session: true,
   }),
   (req, res) => {
+    const token = Math.random()
+    res.cookie('token', token, {
+      httpOnly:true,
+      maxAge:3600000*5,
+      secure:true,
+      sameSite:'none'
+    })
     res.json({ token: req.user.id });
   }
 );
