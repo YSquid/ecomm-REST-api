f6048bc070aa84ed3a15f812e7ac1c20d3e8f27a
Ysquid <ahk648@gmail.com>
2023-02-24 09:39:15 -0600

    remove return from end of local login strat

--- functions/passportConfig.js
+++ functions/passportConfig.js
@@ -39,7 +39,7 @@ passport.use(
         if (!user) return done(null, false);
         const isMatch = await matchPassword(password, user.password);
         if (!isMatch) return done(null, false);
-        return done(null, user);
+        done(null, user);
       } catch (error) {
         return done(error, false);
       }
